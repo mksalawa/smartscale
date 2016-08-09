d5f7d785ed5fdf3eabd06fab2d4e9fd2fa5fe172
Malgorzata Salawa <mksalawa@gmail.com>
2016-08-09 22:55:17 +0200

    SMARTSCALE-18 | Simple AWS command emitter implementation.

--- src/main/java/pl/edu/agh/smartscale/command/AWSCommandEmitter.java
+++ src/main/java/pl/edu/agh/smartscale/command/AWSCommandEmitter.java
@@ -1,10 +1,18 @@
 package pl.edu.agh.smartscale.command;
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
-import com.amazonaws.services.cloudwatch.model.*;
+import com.amazonaws.services.cloudwatch.model.ComparisonOperator;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricAlarmRequest;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
+import com.amazonaws.services.cloudwatch.model.Statistic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class AWSCommandEmitter implements CommandEmitter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AWSCommandEmitter.class);
 
     private final AmazonCloudWatch cloudWatch;
     private final String namespace = "smartscale";
@@ -17,15 +25,32 @@ public class AWSCommandEmitter implements CommandEmitter {
 
     @Override
     public void emit(Command command) {
-        PutMetricDataRequest putMetricDataRequest = new PutMetricDataRequest().withNamespace(namespace);
+        switch (command) {
+            case SCALE_UP:
+                emitScaleUp();
+                break;
+            case SCALE_DOWN:
+                emitScaleDown();
+                break;
+            default:
+                logger.error("Command %s not supported.", command);
+        }
+    }
 
-        putMetricDataRequest.getMetricData().add(
-            new MetricDatum()
-                .withMetricName(metricName)
-                .withValue(1.0)
-        );
+    private void emitScaleUp() {
+        cloudWatch.putMetricData(createPutMetricDataRequest(metricName, namespace, 1.0));
+    }
 
-        cloudWatch.putMetricData(putMetricDataRequest);
+    private void emitScaleDown() {
+        cloudWatch.putMetricData(createPutMetricDataRequest(metricName, namespace, 0));
+    }
+
+    private PutMetricDataRequest createPutMetricDataRequest(String name, String ns, double value) {
+        return new PutMetricDataRequest()
+            .withNamespace(ns)
+            .withMetricData(new MetricDatum()
+                .withMetricName(name)
+                .withValue(value));
     }
 
     private void createAlarm() {
